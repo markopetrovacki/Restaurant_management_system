e0532a2b3199cc31564590d9c1e055dea3114c68
markopetrovacki <petrovacki9@gmail.com>
2026-06-16 22:44:52 +0200

    Initial project structure

---
+++ notification-service/build.gradle.kts
@@ -0,0 +1,43 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.5.15"
+	id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com.restaurant"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(17)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+extra["springCloudVersion"] = "2025.0.3"
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.cloud:spring-cloud-starter-config")
+	implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
+	compileOnly("org.projectlombok:lombok")
+	annotationProcessor("org.projectlombok:lombok")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testCompileOnly("org.projectlombok:lombok")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	testAnnotationProcessor("org.projectlombok:lombok")
+}
+
+dependencyManagement {
+	imports {
+		mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ restaurant-composite-service/src/main/java/com/restaurant/restaurant_composite_service/RestaurantCompositeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.restaurant.restaurant_composite_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantCompositeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantCompositeServiceApplication.class, args);
+	}
+
+}
